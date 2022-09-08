87d6749b77ebdad84f2384f84c7f9aa1d6f5582a
SudarsonAM <maskfamily100@gmail.com>
2022-09-08 22:23:58 +0530

    Secondu murder

--- javascript.js
+++ javascript.js
@@ -1,13 +1,13 @@
 let upcount=0;
-var img=document.getElementById("img");
 function up()
 {
+    var img=document.getElementById("img");
     if(upcount == 0){
-        img.src="https://thumbs.dreamstime.com/b/animals-farm-imaga-cows-57225867.jpg";
+        img.src="./img/Without_Mask.jpeg";
         upcount=1;
     }
-    if(upcount == 1){
-        img.src="https://i.pinimg.com/474x/50/20/c1/5020c1390a26e1f6eed943a59498b27e.jpg";
+    else{
+        img.src="./img/With_Mask.jpeg";
         upcount=0;
     }
     console.log(upcount);
